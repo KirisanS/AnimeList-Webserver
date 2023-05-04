6681c96802ad60c0799efe659f4465ac5e0caf13
ksuthant <ksuthant@uoguelph.ca>
2023-05-04 14:21:33 -0400

    Changed style of Server to make use of Flask Module

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
-import sys;
-import sql
-import json
-import urllib
-from http.server import HTTPServer 
-from http.server import BaseHTTPRequestHandler
+from flask import Flask
 
+app = Flask(__name__)
 
+#Members API Route
+@app.route("/members")
+
+def members():
+    return{"members": ["Member1", "Member2", "Member3"]}
+
+if __name__ == "__main__":
+    app.run(debug=True)
