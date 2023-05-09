60f49b4bdd72be64c2c97775a0110e1fb5a49b26
ksuthant <ksuthant@uoguelph.ca>
2023-05-08 21:31:07 -0400

    Created Basic Home Page with Navigation bar

--- my-app/src/pages/home.jsx
+++ my-app/src/pages/home.jsx
@@ -1,5 +1,24 @@
+import './home.css';
+
 function home() {
-    return <h1> This is the Home Header</h1>
+    return (
+    <html>
+        <body>
+        <header>
+            <div className="container">
+                <nav>
+                    <ul> 
+                        <li><a href="#1">Home</a></li>
+                        <li><a href="#2">About</a></li>
+                        <li><a href="#3">Anime</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        </body>
+    </html>
+        
+    );
 }
 
 export default home;
